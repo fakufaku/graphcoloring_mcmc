472c69818f17ac1c32028a80451a949099e303ab
Robin Scheibler <fakufaku@gmail.com>
2016-05-24 17:42:48 +0200

    Updated pymcmc.cpp for new parameters.

--- pymcmc.cpp
+++ pymcmc.cpp
@@ -19,7 +19,8 @@ color_graph (PyObject *dummy, PyObject *args)
 {
     PyObject *arg1=NULL, *out=NULL, *out2=NULL, *out3=NULL, *ret=NULL;
     PyArrayObject *arr1=NULL, *oarr=NULL, *barr=NULL, *earr=NULL;
-    int q = 5;
+    int q = 5, param_int = 0;
+    double param_double = 0.;
     unsigned long num_iter = 1000;
     int energy = -1;
     int arr1_nd, oarr_nd, barr_nd, earr_nd;
@@ -41,8 +42,12 @@ color_graph (PyObject *dummy, PyObject *args)
     std::default_random_engine generator(seed);
 
     // Parse the input arguments of the function
-    if (!PyArg_ParseTuple(args, "OikO!O!O!", &arg1, &q, &num_iter,
-        &PyArray_Type, &out, &PyArray_Type, &out2, &PyArray_Type, &out3)) return NULL;
+    if (!PyArg_ParseTuple(args, "OikO!O!O!id", 
+          &arg1, &q, &num_iter,
+        &PyArray_Type, &out, 
+        &PyArray_Type, &out2, 
+        &PyArray_Type, &out3,
+        &param_int, &param_double)) return NULL;
 
     // First argument is the adjacency matrix of a graph
     arr1 = (PyArrayObject*)PyArray_FROM_OTF(arg1, NPY_INT16, NPY_IN_ARRAY);
@@ -140,7 +145,8 @@ color_graph (PyObject *dummy, PyObject *args)
     G = new Graph(graph_size, adjacency, q, generator);
 
     // Run Simulated annealing
-    mcmc = new  MCMC(G, q, generator, energy_history, beta_history);
+    mcmc = new  MCMC(G, q, generator, energy_history, beta_history, 
+                     param_int, param_double);
     mcmc->run(num_iter);
 
     // and the energy
